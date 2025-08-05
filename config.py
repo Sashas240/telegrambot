72774d9289933276652a13235a00b39488f2a525
Sashas240 <ruslanbortnik70@gmail.com>
2025-08-05 17:11:59 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-BOT_TOKEN = "8329739889:AAGzFsLgqYUFd7z-WFXTtOPtlCWQZ1XTzTM"
-ADMIN_ID = 5503207753
-GPT_PROMPT = "Ты бизнес-бот в Telegramm (@Giftmad_XVCENbot). Ты должен отвечать, когда пользователь не в сети. Отвечай кратко и без форматирования текста.\nЗапрос: "
